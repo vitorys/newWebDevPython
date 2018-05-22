a6eb1c25de83c5ef37b5d3c5a59e313219ab4d18
Vitor Yudi Shinohara <vitor.ysh@gmail.com>
2018-05-22 08:08:55 -0300

    Responses implemented

---
+++ Responses.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+response404 = "HTTP/1.1 404 Not Found\r\n\r\n";
+response200 = "HTTP/1.1 200 OK\r\n"
+response401 = "\"HTTP/1.1 401 Unauthorized\r\nDate: Wed, 21 Oct 2015 07:28:00 GMT" + "\r\nWWW-Authenticate: Basic realm=\\\"Access to staging site\\\"\r\n\r\n";
--- Worker.py
+++ Worker.py
@@ -1,5 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
 from threading import Thread
 from Header import Header
+from Responses import *
 
 class Worker(Thread):
 	"""docstring for Worker"""
@@ -15,7 +20,7 @@ class Worker(Thread):
 		requestHeader = self.parseHeader()
 
 		response = self.buildResponse(requestHeader)
-
+		print response
 		self.client_connection.sendall(response.encode())
 
 		self.client_connection.close()
@@ -24,9 +29,11 @@ class Worker(Thread):
 	def parseHeader(self):
 		header = self.client_request.split("\n")
 		
-		firstLineSplited = header[0].split(" ")
+		firstLineSplited = header[0].encode().split(" ")
 		method = firstLineSplited[0]
 		path = firstLineSplited[1]
+		print firstLineSplited
+
 		cookie = None
 
 		for line in header:
@@ -40,8 +47,60 @@ class Worker(Thread):
 		return requestHeader
 
 	def buildResponse(self, requestHeader):
-		return "HTTP/1.0 200 OK\r\n\r\nHello World"
+		# Inicializa variáveis da resposta
+		headerResponse = None
+		contentOfPath = None
+
+		# Buid response header
+		pathExist, headerResponse = self.buildHeaderResponse(requestHeader.path)
+		
+		if pathExist:
+			# Deve verificar se o caminho é diretório ou arquivo
+			if os.path.isfile(requestHeader.path):
+				# Se for arquivo 
+				contentOfPath = self.showFileContent(requestHeader.path)
+			else:
+				# Se for diretório
+				contentOfPath = self.showDirContent(requestHeader.path)
+
+			
+
+		else:
+			# 404 NOT FOUND
+			response = headerResponse
+			return response
+
+		#Build content of response
 
 
+		return headerResponse
+
+	def buildHeaderResponse(self, path):
+		# Build Response Header
+		if os.path.exists(path):
+			response = response200
+		else:
+			response = response404
+			# Retorna flag falando que arquivo não existe e erro 404
+			return (False, response)
+
+		# Retorna flag falando que arquivo existe e código 200
+		return (True, response)
+
+
+	# Thanks to @DanielVenturini
+	def showFileContent(self, path):
+		file = open(path, "rb")
+        bytesSequence = file.read(size)        	# read only 512 bytes in each loop
+        
+        while(bytes.__len__(bytesSequence)):
+            self.send(bytesSequence)	        # send the 512 bytes
+            bytesSequence = file.read(size)    	# get nexts 512 bytes
+
+
+
+
+	def showDirContent(self, path):
+		pass
 
 		
